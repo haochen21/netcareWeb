6e172290b081391aa1633f078a963ea23c5b988f
haochen21 <chenhao21@163.com>
2014-08-22 15:08:53 +0800

    app module

--- test/main.js
+++ test/main.js
@@ -1,9 +1,43 @@
-var assert = require("assert");
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
-        })
-    })
+var app = require('../server').app;
+var assert = require('assert');
+var superagent = require('superagent');
+
+var port = 'http://localhost:' + app.get('port');
+
+app.listen(app.get('port'), function () {
+    console.log('server listening on port ' + app.get('port'));
 });
+
+describe('APPLICATION API', function () {
+    it('login success', function (done) {
+        superagent.post('http://localhost:3000/api/login')
+            .send({loginName: 'admin', password: 'abcd1234'})
+            .end(function (res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.type, 'AUTHORIZED');
+                console.log(res.body.user);
+                done();
+            })
+    })
+
+
+    it('login password error', function (done) {
+        superagent.post('http://localhost:3000/api/login')
+            .send({loginName: 'admin', password: 'abcd'})
+            .end(function (res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.type, 'PASSWORDERROR');
+                done();
+            })
+    })
+
+    it('login loginName error', function (done) {
+        superagent.post('http://localhost:3000/api/login')
+            .send({loginName: 'errorName', password: 'abcd1234'})
+            .end(function (res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.type, 'LOGINNAMEERROR');
+                done();
+            })
+    })
+})
