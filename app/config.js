bbf99fbbc733bcecaa1be3a091a0d147ee3be981
haochen21 <chenhao21@163.com>
2014-08-22 14:16:03 +0800

    app module

---
+++ app/config.js
@@ -0,0 +1,11 @@
+exports.Mongodb = {
+    url: "mongodb://127.0.0.1:27017/netcare"
+}
+
+exports.Amqp = {
+    host: '127.0.0.1',
+    port: 5672,
+    login: 'netcare',
+    password: 'netcare',
+    vhost: 'netcare-vhost'
+}	
