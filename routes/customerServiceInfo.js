024ded8d7d49c85626d791224d8e4985dc69e52e
haochen21 <chenhao21@163.com>
2015-02-02 14:16:50 +0800

    v0.1

---
+++ routes/customerServiceInfo.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+exports.downloadFile  = function(req,res,next){
+    var options = {
+        root: path.join(__dirname, '..','public','assets','cusfile',req.params.customerGroupId,req.params.type),
+        dotfiles: 'ignore',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    var fileName = req.params.fileName;
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            if (err.code === "ECONNABORT" && res.statusCode == 304) {
+                // No problem, 304 means client cache hit, so no data sent.
+                console.log('304 cache hit for ' + fileName);
+                return;
+            }
+            console.error("SendFile error:", err, " (status: " + err.status + ")");
+            if (err.status) {
+                res.status(err.status).end();
+            }
+        }
+        else {
+            console.log('Sent:', fileName);
+        }
+    });
+};
